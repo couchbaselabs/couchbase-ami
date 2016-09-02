219bd1326a18c8baa915d420d4739cd5deb1c8c5
sandipnd <sandip.nandi@couchbase.com>
2016-09-01 18:27:37 -0700

    new changes

--- Couchbaseinit.sh
+++ Couchbaseinit.sh
@@ -185,10 +185,10 @@ for id in ${INSTANCES_IN_ASG}
 do
 	MYROLE=${INSTANCE2ROLE[${id}]}
 	if [ "${MYROLE}" == "master" ]; then
-		#curl -v -X POST http://$MASTER_DNS:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
+		curl -v -X POST http://$MASTER_DNS:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
 		curl -v http://$MASTER_DNS:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
 		curl -v http://$MASTER_DNS:8091/settings/web -d port=8091 -d username=${ADMIN} -d password=${PASSWORD}
-		curl -v -u ${ADMIN}:${PASSWORD} -X POST http://$MASTER_DNS:8091/sampleBuckets/install -d '["travel-sample"]'		
+		#curl -v -u ${ADMIN}:${PASSWORD} -X POST http://$MASTER_DNS:8091/sampleBuckets/install -d '["travel-sample"]'		
 	else
 		WORKER_DNS=${INSTANCE2DNS[${id}]}
 		WORKER_IP=${INSTANCE2IP[${id}]}
@@ -213,7 +213,7 @@ do
 	KNOWN_NODES="ns_1@"$(echo ${MYIP}),${KNOWN_NODES}
 done
 
-sleep 60
+sleep 120
 COMMAND=$(echo curl -v -u ${ADMIN}:${PASSWORD} -X POST "http://$MASTER_DNS:8091/controller/rebalance" -d  "knownNodes=${KNOWN_NODES}")
 echo ${COMMAND}
 ${COMMAND}
